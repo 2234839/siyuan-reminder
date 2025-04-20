ef90635e5f912fc40a909ff1eead0e1e5b7526b3
崮生（子虚） <2234839456@qq.com>
2025-04-20 11:08:37 +0000

    fix 未存储

--- src/config.ts
+++ src/config.ts
@@ -6,11 +6,12 @@ const store_key = 'gm_reminder';
 const server = gm_ref(store_key + 'server', 'http://127.0.0.1:6806');
 const token = gm_ref(store_key + 'token', '');
 const protocol = gm_ref<'siyuan' | 'http'>(store_key + 'protocol', 'siyuan');
+const queryWebContent = gm_ref<boolean>(store_key + 'protoqueryWebContentcol', false);
 
 export const config = reactive({
   server,
   token,
   protocol,
   /** 是否解析正文内容 */
-  queryWebContent:false
+  queryWebContent
 });
